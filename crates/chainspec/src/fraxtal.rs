e82363620bad574ab31331a5d7ded0934eac3306
Alessandro Tagliapietra <0xalex88@proton.me>
2025-03-31 21:39:34 -0700

    Declare genesis header in chainspec

--- crates/chainspec/src/fraxtal.rs
+++ crates/chainspec/src/fraxtal.rs
@@ -3,89 +3,31 @@
 use std::sync::{Arc, LazyLock};
 
 use alloy_chains::Chain;
+use alloy_primitives::b256;
 use alloy_primitives::U256;
 use reth_chainspec::{BaseFeeParams, BaseFeeParamsKind, ChainHardforks, ChainSpec, ForkCondition};
 use reth_ethereum_forks::EthereumHardfork;
 use reth_ethereum_forks::Hardfork;
+use reth_optimism_chainspec::make_op_genesis_header;
 use reth_optimism_chainspec::OpChainSpec;
 use reth_optimism_forks::OpHardfork;
+use reth_primitives_traits::SealedHeader;
 
 /// The Base mainnet spec
 pub(crate) static FRAXTAL_MAINNET: LazyLock<Arc<OpChainSpec>> = LazyLock::new(|| {
+    let genesis = serde_json::from_str(include_str!("../res/genesis/mainnet.json"))
+        .expect("Can't deserialize Fraxtal genesis json");
+    let hardforks = FRAXTAL_MAINNET_HARDFORKS.clone();
     OpChainSpec {
         inner: ChainSpec {
             chain: Chain::fraxtal(),
-            genesis: serde_json::from_str(include_str!("../res/genesis/mainnet.json"))
-                .expect("Can't deserialize Fraxtal genesis json"),
+            genesis_header: SealedHeader::new(
+                make_op_genesis_header(&genesis, &hardforks),
+                b256!("0x521982bd54239dc71269eefb58601762cc15cfb2978e0becb46af7962ed6bfaa"),
+            ),
+            genesis,
             paris_block_and_final_difficulty: Some((0, U256::from(0))),
-            hardforks: ChainHardforks::new(vec![
-                (EthereumHardfork::Frontier.boxed(), ForkCondition::Block(0)),
-                (EthereumHardfork::Homestead.boxed(), ForkCondition::Block(0)),
-                (EthereumHardfork::Tangerine.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::SpuriousDragon.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (EthereumHardfork::Byzantium.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::Constantinople.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (
-                    EthereumHardfork::Petersburg.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (EthereumHardfork::Istanbul.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::MuirGlacier.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (EthereumHardfork::Berlin.boxed(), ForkCondition::Block(0)),
-                (EthereumHardfork::London.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::ArrowGlacier.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (
-                    EthereumHardfork::GrayGlacier.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (
-                    EthereumHardfork::Paris.boxed(),
-                    ForkCondition::TTD {
-                        activation_block_number: 0,
-                        fork_block: Some(0),
-                        total_difficulty: U256::ZERO,
-                    },
-                ),
-                (OpHardfork::Bedrock.boxed(), ForkCondition::Block(0)),
-                (OpHardfork::Regolith.boxed(), ForkCondition::Timestamp(0)),
-                (
-                    EthereumHardfork::Shanghai.boxed(),
-                    ForkCondition::Timestamp(0),
-                ),
-                (OpHardfork::Canyon.boxed(), ForkCondition::Timestamp(0)),
-                (
-                    EthereumHardfork::Cancun.boxed(),
-                    ForkCondition::Timestamp(1717009201),
-                ),
-                (
-                    OpHardfork::Ecotone.boxed(),
-                    ForkCondition::Timestamp(1717009201),
-                ),
-                (
-                    OpHardfork::Fjord.boxed(),
-                    ForkCondition::Timestamp(1733947201),
-                ),
-                (
-                    OpHardfork::Granite.boxed(),
-                    ForkCondition::Timestamp(1738958401),
-                ),
-                (
-                    OpHardfork::Holocene.boxed(),
-                    ForkCondition::Timestamp(1744052401),
-                ),
-            ]),
+            hardforks,
             base_fee_params: BaseFeeParamsKind::Variable(
                 vec![
                     (EthereumHardfork::London.boxed(), BaseFeeParams::optimism()),
@@ -97,4 +39,75 @@ pub(crate) static FRAXTAL_MAINNET: LazyLock<Arc<OpChainSpec>> = LazyLock::new(||
         },
     }
     .into()
+});
+
+static FRAXTAL_MAINNET_HARDFORKS: LazyLock<ChainHardforks> = LazyLock::new(|| {
+    ChainHardforks::new(vec![
+        (EthereumHardfork::Frontier.boxed(), ForkCondition::Block(0)),
+        (EthereumHardfork::Homestead.boxed(), ForkCondition::Block(0)),
+        (EthereumHardfork::Tangerine.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::SpuriousDragon.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (EthereumHardfork::Byzantium.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::Constantinople.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (
+            EthereumHardfork::Petersburg.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (EthereumHardfork::Istanbul.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::MuirGlacier.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (EthereumHardfork::Berlin.boxed(), ForkCondition::Block(0)),
+        (EthereumHardfork::London.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::ArrowGlacier.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (
+            EthereumHardfork::GrayGlacier.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (
+            EthereumHardfork::Paris.boxed(),
+            ForkCondition::TTD {
+                activation_block_number: 0,
+                fork_block: Some(0),
+                total_difficulty: U256::ZERO,
+            },
+        ),
+        (OpHardfork::Bedrock.boxed(), ForkCondition::Block(0)),
+        (OpHardfork::Regolith.boxed(), ForkCondition::Timestamp(0)),
+        (
+            EthereumHardfork::Shanghai.boxed(),
+            ForkCondition::Timestamp(0),
+        ),
+        (OpHardfork::Canyon.boxed(), ForkCondition::Timestamp(0)),
+        (
+            EthereumHardfork::Cancun.boxed(),
+            ForkCondition::Timestamp(1717009201),
+        ),
+        (
+            OpHardfork::Ecotone.boxed(),
+            ForkCondition::Timestamp(1717009201),
+        ),
+        (
+            OpHardfork::Fjord.boxed(),
+            ForkCondition::Timestamp(1733947201),
+        ),
+        (
+            OpHardfork::Granite.boxed(),
+            ForkCondition::Timestamp(1738958401),
+        ),
+        (
+            OpHardfork::Holocene.boxed(),
+            ForkCondition::Timestamp(1744052401),
+        ),
+    ])
 });
--- crates/chainspec/src/fraxtal_testnet.rs
+++ crates/chainspec/src/fraxtal_testnet.rs
@@ -3,89 +3,31 @@
 use std::sync::{Arc, LazyLock};
 
 use alloy_chains::Chain;
+use alloy_primitives::b256;
 use alloy_primitives::U256;
 use reth_chainspec::{BaseFeeParams, BaseFeeParamsKind, ChainHardforks, ChainSpec, ForkCondition};
 use reth_ethereum_forks::EthereumHardfork;
 use reth_ethereum_forks::Hardfork;
+use reth_optimism_chainspec::make_op_genesis_header;
 use reth_optimism_chainspec::OpChainSpec;
 use reth_optimism_forks::OpHardfork;
+use reth_primitives_traits::SealedHeader;
 
 /// The Base mainnet spec
 pub(crate) static FRAXTAL_TESTNET: LazyLock<Arc<OpChainSpec>> = LazyLock::new(|| {
+    let genesis = serde_json::from_str(include_str!("../res/genesis/testnet.json"))
+        .expect("Can't deserialize Fraxtal testnet genesis json");
+    let hardforks = FRAXTAL_TESTNET_HARDFORKS.clone();
     OpChainSpec {
         inner: ChainSpec {
             chain: Chain::fraxtal_testnet(),
-            genesis: serde_json::from_str(include_str!("../res/genesis/testnet.json"))
-                .expect("Can't deserialize Fraxtal testnet genesis json"),
+            genesis_header: SealedHeader::new(
+                make_op_genesis_header(&genesis, &hardforks),
+                b256!("0x910f5c4084b63fd860d0c2f9a04615115a5a991254700b39ba072290dbd77489"),
+            ),
+            genesis,
             paris_block_and_final_difficulty: Some((0, U256::from(0))),
-            hardforks: ChainHardforks::new(vec![
-                (EthereumHardfork::Frontier.boxed(), ForkCondition::Block(0)),
-                (EthereumHardfork::Homestead.boxed(), ForkCondition::Block(0)),
-                (EthereumHardfork::Tangerine.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::SpuriousDragon.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (EthereumHardfork::Byzantium.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::Constantinople.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (
-                    EthereumHardfork::Petersburg.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (EthereumHardfork::Istanbul.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::MuirGlacier.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (EthereumHardfork::Berlin.boxed(), ForkCondition::Block(0)),
-                (EthereumHardfork::London.boxed(), ForkCondition::Block(0)),
-                (
-                    EthereumHardfork::ArrowGlacier.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (
-                    EthereumHardfork::GrayGlacier.boxed(),
-                    ForkCondition::Block(0),
-                ),
-                (
-                    EthereumHardfork::Paris.boxed(),
-                    ForkCondition::TTD {
-                        activation_block_number: 0,
-                        fork_block: Some(0),
-                        total_difficulty: U256::ZERO,
-                    },
-                ),
-                (OpHardfork::Bedrock.boxed(), ForkCondition::Block(0)),
-                (OpHardfork::Regolith.boxed(), ForkCondition::Timestamp(0)),
-                (
-                    EthereumHardfork::Shanghai.boxed(),
-                    ForkCondition::Timestamp(0),
-                ),
-                (OpHardfork::Canyon.boxed(), ForkCondition::Timestamp(0)),
-                (
-                    EthereumHardfork::Cancun.boxed(),
-                    ForkCondition::Timestamp(1714186800),
-                ),
-                (
-                    OpHardfork::Ecotone.boxed(),
-                    ForkCondition::Timestamp(1714186800),
-                ),
-                (
-                    OpHardfork::Fjord.boxed(),
-                    ForkCondition::Timestamp(1730401200),
-                ),
-                (
-                    OpHardfork::Granite.boxed(),
-                    ForkCondition::Timestamp(1738191600),
-                ),
-                (
-                    OpHardfork::Holocene.boxed(),
-                    ForkCondition::Timestamp(1742583600),
-                ),
-            ]),
+            hardforks,
             base_fee_params: BaseFeeParamsKind::Variable(
                 vec![
                     (EthereumHardfork::London.boxed(), BaseFeeParams::optimism()),
@@ -97,4 +39,75 @@ pub(crate) static FRAXTAL_TESTNET: LazyLock<Arc<OpChainSpec>> = LazyLock::new(||
         },
     }
     .into()
+});
+
+static FRAXTAL_TESTNET_HARDFORKS: LazyLock<ChainHardforks> = LazyLock::new(|| {
+    ChainHardforks::new(vec![
+        (EthereumHardfork::Frontier.boxed(), ForkCondition::Block(0)),
+        (EthereumHardfork::Homestead.boxed(), ForkCondition::Block(0)),
+        (EthereumHardfork::Tangerine.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::SpuriousDragon.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (EthereumHardfork::Byzantium.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::Constantinople.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (
+            EthereumHardfork::Petersburg.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (EthereumHardfork::Istanbul.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::MuirGlacier.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (EthereumHardfork::Berlin.boxed(), ForkCondition::Block(0)),
+        (EthereumHardfork::London.boxed(), ForkCondition::Block(0)),
+        (
+            EthereumHardfork::ArrowGlacier.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (
+            EthereumHardfork::GrayGlacier.boxed(),
+            ForkCondition::Block(0),
+        ),
+        (
+            EthereumHardfork::Paris.boxed(),
+            ForkCondition::TTD {
+                activation_block_number: 0,
+                fork_block: Some(0),
+                total_difficulty: U256::ZERO,
+            },
+        ),
+        (OpHardfork::Bedrock.boxed(), ForkCondition::Block(0)),
+        (OpHardfork::Regolith.boxed(), ForkCondition::Timestamp(0)),
+        (
+            EthereumHardfork::Shanghai.boxed(),
+            ForkCondition::Timestamp(0),
+        ),
+        (OpHardfork::Canyon.boxed(), ForkCondition::Timestamp(0)),
+        (
+            EthereumHardfork::Cancun.boxed(),
+            ForkCondition::Timestamp(1714186800),
+        ),
+        (
+            OpHardfork::Ecotone.boxed(),
+            ForkCondition::Timestamp(1714186800),
+        ),
+        (
+            OpHardfork::Fjord.boxed(),
+            ForkCondition::Timestamp(1730401200),
+        ),
+        (
+            OpHardfork::Granite.boxed(),
+            ForkCondition::Timestamp(1738191600),
+        ),
+        (
+            OpHardfork::Holocene.boxed(),
+            ForkCondition::Timestamp(1742583600),
+        ),
+    ])
 });
